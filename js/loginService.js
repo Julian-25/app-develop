4fd7c1c68914547bc384323d1f66032f4c10cd3f
Julian-25 <jjpedrazab@ufpso.edu.co>
2025-04-08 20:11:04 -0500

    Edicion de js2

--- js/loginService.js
+++ js/loginService.js
@@ -3,5 +3,7 @@ document.getElementById('loginForm').addEventListener('submit', function (e) {
     const email = document.getElementById('email').value;
     alert();
     const password = document.getElementById('password').value;
-    alert('Hola estos son tus datos: email: ' +email +' password: ' +password);
+    if(email === '' && password === ''){
+        console.log('Por favor completa todos los campos')
+    }
 });

e62da09b421890681d4320228974bffe5289d0bd
Julian-25 <jjpedrazab@ufpso.edu.co>
2025-05-06 19:14:51 -0500

    creacion de nuevo js dashboardService

---
+++ js/dashboardService.js
@@ -0,0 +1,12 @@
+function users() {
+    document.getElementById('info') = '<h1>Lista de usuarios</h1>'
+}
+
+function products() {
+    document.getElementById('info') = '<h1>Lista de productos</h1>'
+}
+
+function logout() {
+    localStorage.removeItem('token')
+    location.href =  '../index.html'
+}

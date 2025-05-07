5fe2722b59f1a90bbc3c27a0caa378e4841ab8d8
Julian-25 <jjpedrazab@ufpso.edu.co>
2025-05-06 19:22:41 -0500

    arreglos

--- js/dashboardService.js
+++ js/dashboardService.js
@@ -1,9 +1,9 @@
 function users() {
-    document.getElementById('info') = '<h1>Lista de usuarios</h1>'
+    document.getElementById('info').innerHTML = '<h1>Lista de usuarios</h1>'
 }
 
 function products() {
-    document.getElementById('info') = '<h1>Lista de productos</h1>'
+    document.getElementById('info').innerHTML = '<h1>Lista de productos</h1>'
 }
 
 function logout() {
